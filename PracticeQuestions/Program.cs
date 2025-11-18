ce69fb9a0a89d533c82aab110e1d85cbcf223c0e
Shreyabt08 <shreya@braintechnosys.biz>
2025-11-18 23:29:42 +0530

    ReverseString

--- PracticeQuestions/Program.cs
+++ PracticeQuestions/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PracticeQuestions;
 using PracticeQuestions.OOPs;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine();
 Classes car = new Classes();
 car.TestCar();
+
+ReverseString reverseString = new ReverseString();
+Console.WriteLine(reverseString.Reverse("Shreya"));
---
+++ PracticeQuestions/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQuestions
+{
+    public class ReverseString
+    {
+        public string Reverse(string name)
+        {
+            char[] c = name.ToCharArray();
+            int n = c.Length;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                (c[i], c[n - 1 - i]) = (c[n - 1 - i], c[i]);
+            }
+
+            return new string(c);
+        }
+    }
+
+}
